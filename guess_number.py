cd8c2a0fb9ba5f4adf47932ab1626709dfbd2de8
Diana Shakirova <diana.shak1rova@yandex.ru>
2024-11-18 21:11:32 +0300

    First version of file

---
+++ guess_number.py
@@ -0,0 +1,18 @@
+from random import randint
+
+
+
+NOT_GUESSED = True
+computer_number = randint(1, 100)
+
+
+while not_guessed:
+    print('Enter yout number:')
+    user_number = int(input())
+    if computer_number == user_number:
+        print('Great intuition! You guessed the number!')
+        not_guessed = False
+    elif user_number > computer_number:
+        print('Your number is more than computer one. Try again!') 
+    else:
+        print('Yor number is less than computer one. Try again!')
